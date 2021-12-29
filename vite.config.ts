200d66fe1505694ce8f9d2428aedd766f32f3735
tangyy <yueyangtang@163.com>
2021-12-29 17:50:45 +0800

    Mod alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { injectHtml } from 'vite-plugin-html'
 import { loadEnv } from 'vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => defineConfig({
@@ -12,5 +13,10 @@ export default ({ mode }) => defineConfig({
         title: loadEnv(mode, process.cwd()).VITE_APP_TITLE
       }
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname,'src')
+    }
+  }
 })
